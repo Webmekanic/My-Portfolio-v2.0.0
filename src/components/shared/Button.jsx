fad08fb2e271177e20c10c7832cd2fe4b2a5fe99
aniebodavid <aniebovictor001@gmail.com>
2022-08-09 09:12:21 -0700

    feat:: add button animation

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -4,10 +4,10 @@ import styled from "styled-components"
 const PageButton = styled.button`
   border: 2px solid red;
   display: inline-block;
+  padding: 0.9375rem 1.875rem;
   font-size: 14px;
   letter-spacing: 0.0938rem;
   text-transform: capitalize;
-  padding: 0.9375rem 1.875rem;
   min-width: 150px;
   background: transparent;
   cursor: pointer;
@@ -15,12 +15,33 @@ const PageButton = styled.button`
   font-style: regular;
   font-weight: 400;
   border: 2px solid ${({ theme }) => theme.colors.lightColor};
-  color: ${({ theme }) => theme.colors.lightColor};
   margin: 30px auto;
+  color: ${({ theme }) => theme.colors.lightColor};
+  position: relative;
+  margin-left: 5rem;
+  overflow: hidden;
+
+  .buttonText {
+    position: absolute;
+    border: 2px solid red;
+    top: 0;
+    left: -50px;
+    right: 40px;
+    bottom: 0;
+    transform: skew(-62deg);
+    overflow: hidden;
+    transition: 0.3s ease-out;
+    background-color: ${({ theme }) => theme.colors.lightColor};
+  }
 `
 
 function Button({ text }) {
-  return <PageButton>{text}</PageButton>
+  return (
+    <PageButton>
+      {text}
+      <div className="buttonText"></div>
+    </PageButton>
+  )
 }
 
 export default Button
--- src/themes/GlobalStyles.jsx
+++ src/themes/GlobalStyles.jsx
@@ -13,7 +13,7 @@ html{
 }
 body{
   color: white;
-  // background: #0C1114;
+  background: #0C1114;
   font-family: 'Work Sans', sans-serif;
 
 }
