a4c5a779e9bd39603935febb74658755001ae461
aniebodavid <aniebovictor001@gmail.com>
2022-08-21 11:22:18 -0700

    feat:: add about components

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,6 +7,24 @@ const About = () => {
       <div className="pattern1"></div>
       <div className="pattern2"></div>
       <div className="pattern3"></div>
+      <section>
+        <h1 className="aboutTitle">A bit about me</h1>
+        <div>
+          <p>
+            I am a Front-end Developer with skills in HTML, CSS, Bootstrap SaSS
+            and ReactJS, passionate about developing clean, intuitive interfaces
+            and adding my bit to the world of web development.
+          </p>
+          <p>
+            When I am not knee-deep into programming, I can be found reading
+            playing games or going for a walk with my dogs.
+          </p>
+          <p>
+            I am open to freelance and full-time positions in an environment
+            which presents fun challenges with provision for constant growth.
+          </p>
+        </div>
+      </section>
     </AboutMe>
   )
 }
