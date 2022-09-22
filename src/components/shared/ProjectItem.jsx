6c467db170ea3129d874789725425f86c27aacfb
aniebodavid <aniebovictor001@gmail.com>
2022-09-22 14:55:58 -0700

    fix:: add styling to project card

--- src/components/shared/ProjectItem.jsx
+++ src/components/shared/ProjectItem.jsx
@@ -1,17 +1,43 @@
 import React from "react"
 import { ProjectItemStyle } from "../../styles/ProjectItemStyle"
+import { FiGithub, FiExternalLink } from "react-icons/fi"
 
 const ProjectItem = () => {
   return (
     <ProjectItemStyle>
       <div className="projectBg"></div>
-      <div className="projectDescription">
-        <p>SureVoter</p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat ullam
-          pariatur nesciunt maiores! Voluptatem, deleniti quasi. Totam ducimus
-          facilis omnis!
+      <div className="projectSummary">
+        <p className="projectTitle">Advice generator app</p>
+        <div className="skillsTag"></div>
+        <p className="projectDescription">
+          The perfect project if you're learning how to interact with 3rd-party
+          APIs. This challenge uses the Advice Slip API to generate random
+          quotes of advice.
         </p>
+        <div className="projectLinks">
+          <button>
+            Repo
+            <a
+              className="socialLink"
+              href="https://github.com/Webmekanic"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FiGithub />
+            </a>
+          </button>
+          <button>
+            Live View
+            <a
+              className="socialLink"
+              href="https://github.com/Webmekanic"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FiExternalLink />
+            </a>
+          </button>
+        </div>
       </div>
     </ProjectItemStyle>
   )
--- src/styles/ProjectItemStyle.jsx
+++ src/styles/ProjectItemStyle.jsx
@@ -1,12 +1,13 @@
 import styled from "styled-components"
 
 export const ProjectItemStyle = styled.div`
-  border: 1px solid ${({ theme }) => theme.colors.purple};
+  border: 1px solid #414141;
   width: 350px;
   height: auto;
   padding: 15px;
   border-radius: 5px;
   background-color: #090d0f;
+  cursor: pointer;
 
   .projectBg {
     height: 200px;
@@ -14,8 +15,90 @@ export const ProjectItemStyle = styled.div`
     background-color: #485dab;
     border-radius: 3px;
   }
-  .projectDescription {
-    border: 2px solid yellow;
+  .projectSummary {
     margin-top: 10px;
+  }
+  .projectTitle {
+    color: ${({ theme }) => theme.colors.neutral};
+    font-size: 20px;
+    font-family: Chivo;
+    font-style: black;
+    font-weight: 600;
+    letter-spacing: 1px;
+  }
+
+  .skillsTag {
+    // border: 1px solid red;
+    display: flex;
+    flex-direction: row;
+    grid-gap: 0.5rem;
+  }
+
+  .projectDescription {
+    // border: 2px solid purple;
+    margin-top: 5px;
+    font-family: Chivo;
+    font-style: black;
+    line-height: 1.5;
+    max-height: 60%;
+  }
+  .projectLinks {
+    margin-top: 0.6rem;
+    display: flex;
+    text-align: center;
+    align-items: center;
+    grid-gap: 1rem;
+
+    button {
+      display: flex;
+      text-align: center;
+      align-items: center;
+      justify-content: center;
+      padding: 0.2rem 0.3rem;
+      border: 2px solid red;
+      grid-gap: 0.4rem;
+      border: none;
+      border-radius: 3px;
+    }
+  }
+  .socialLink {
+    // border: 2px solid red;
+    // background-color: ${({ theme }) => theme.colors.lightColor};
+  }
+
+  .skill1 {
+    width: auto;
+    height: 15px;
+    position: relative;
+    background: white;
+    border-left: none;
+    font-size: 11px;
+    font-weight: bold;
+    padding-left: 10px;
+    color: purple;
+
+    &:after {
+      content: "";
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      width: 0;
+      height: 0;
+      border-left: 7.5px solid black;
+      border-top: 7.5px solid transparent;
+      border-bottom: 7.5px solid transparent;
+    }
+
+    &:before {
+      content: "";
+      position: absolute;
+      right: -7.5px;
+      bottom: 0;
+      width: 0;
+      height: 0;
+      border-left: 7.5px solid white;
+      border-top: 7.5px solid transparent;
+      border-bottom: 7.5px solid transparent;
+    }
   }
 `
