9dc5b7d75d669ae55c6812b328f9011528190a6d
aniebodavid <aniebovictor001@gmail.com>
2022-11-07 04:00:41 -0800

    fill the total number of array in cardskeleton

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -2,8 +2,9 @@
 import React, { useState, useEffect, useContext } from "react"
 import ProjectItem from "../shared/ProjectItem"
 import Flickity from "react-flickity-component"
-import { MySlider } from "./CarouselStyle"
 import PortfolioContext from "../../context/portfolio/PortfolioContext"
+import CardSkeleton from "../shared/CardSkeleton"
+import { MySlider } from "./CarouselStyle"
 
 const Carousel = (props) => {
   let flkty
@@ -45,6 +46,8 @@ const Carousel = (props) => {
           wrapAround: false,
         }}
       >
+        <CardSkeleton cards={4} />
+
         {/* {projects.length === 0 ? } */}
         {projects.map((project, index) => {
           return (
--- src/components/shared/CardSkeleton.jsx
+++ src/components/shared/CardSkeleton.jsx
@@ -2,33 +2,35 @@ import React from "react"
 import { ProjectItemStyle } from "../../styles/ProjectItemStyle"
 import Skeleton from "react-loading-skeleton"
 
-const CardSkeleton = () => {
-  return (
-    <ProjectItemStyle>
-      <section className="projectContainer">
-        <div className="projectBg">
-          <Skeleton height={180} />
-        </div>
-        <div className="projectSummary">
-          <p className="projectTitle">
-            <Skeleton />
-          </p>
-          <div className="skillsTag"></div>
-          <p className="projectDescription">
-            <Skeleton count={4} />
-          </p>
-          <div className="projectLinks">
-            <div>
-              <Skeleton width={80} height={20} />
-            </div>
-            <div>
-              <Skeleton width={80} height={20} />
+const CardSkeleton = ({ cards }) => {
+  return Array(cards)
+    .fill(0)
+    .map((item, index) => (
+      <ProjectItemStyle key={index}>
+        <section className="projectContainer">
+          <div className="projectBg">
+            <Skeleton height={180} />
+          </div>
+          <div className="projectSummary">
+            <p className="projectTitle">
+              <Skeleton />
+            </p>
+            <div className="skillsTag"></div>
+            <p className="projectDescription">
+              <Skeleton count={4} />
+            </p>
+            <div className="projectLinks">
+              <div>
+                <Skeleton width={80} height={20} />
+              </div>
+              <div>
+                <Skeleton width={80} height={20} />
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-    </ProjectItemStyle>
-  )
+        </section>
+      </ProjectItemStyle>
+    ))
 }
 
 export default CardSkeleton
